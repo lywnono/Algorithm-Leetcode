e03e3c474f89b85660b3d87a906e64709625b8a6
Elaine Lin <lywnono0611@gmail.com>
2025-04-06 22:11:18 +0800

    Create mergeKLists.cpp

---
+++ Week5/mergeKLists.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto cmp = [](ListNode*& a, ListNode*& b) {
+            return a->val > b->val;
+        };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp) > q(cmp);
+        for (auto node : lists) {
+            if (node) q.push(node);
+        }
+        ListNode *dummy = new ListNode(-1), *cur = dummy;
+        while (!q.empty()) {
+            auto t = q.top(); q.pop();
+            cur->next = t;
+            cur = cur->next;
+            if (cur->next) q.push(cur->next);
+        }
+        return dummy->next;
+    }
+};
