af95d974b8558bb44b033b990645c4a25b6a257b
Elaine Lin <lywnono0611@gmail.com>
2025-04-06 22:03:31 +0800

    Create pow.cpp

---
+++ Week4/pow.cpp
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+    
+        if n == 0:
+            return 1.0
+        
+        elif n == 1:
+            return x
+        
+        elif n < 0:
+            return  self.myPow( 1/x, -n)
+        
+        
+        # Recurrence of fast power
+        
+        if n & 1:
+            return x * self.myPow( x*x, n // 2)
+        else:
+            return self.myPow( x*x, n // 2)
